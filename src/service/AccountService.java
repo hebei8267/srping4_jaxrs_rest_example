3125c36977c656b501ca444408308334aa773172
he_bei <he_bei@172.29.225.217>
2014-06-27 10:51:24 +0800

    iiiiiiii

--- src/service/AccountService.java
+++ src/service/AccountService.java
@@ -1,19 +1,11 @@
 package service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import entity.User;
 
 /**
  * 用户管理业务类.
  */
 public class AccountService {
-	public static final String HASH_ALGORITHM = "SHA-1";
-	public static final int HASH_INTERATIONS = 1024;
-	private static final int SALT_SIZE = 8;
-
-	private static Logger logger = LoggerFactory.getLogger(AccountService.class);
 
 	/**
 	 * 按登录名查询用户.
@@ -22,7 +14,7 @@ public class AccountService {
 		User user = new User();
 
 		user.setLoginName("bob");
-		user.setPassword("bob");
+		user.setPassword("bobbob");
 		user.setName("test user");
 		return user;
 	}
--- src/service/ShiroDbRealm.java
+++ src/service/ShiroDbRealm.java
@@ -1,24 +1,14 @@
 package service;
 
-import java.io.Serializable;
-
-import javax.annotation.PostConstruct;
-
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
-import org.apache.shiro.authc.DisabledAccountException;
 import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
-import org.apache.shiro.util.ByteSource;
-import org.springside.modules.utils.Encodes;
-
-import com.google.common.base.Objects;
 
 import entity.Role;
 import entity.User;
@@ -63,77 +53,8 @@ public class ShiroDbRealm extends AuthorizingRealm {
 		return info;
 	}
 
-	/**
-	 * 设定Password校验的Hash算法与迭代次数.
-	 */
-	@PostConstruct
-	public void initCredentialsMatcher() {
-		HashedCredentialsMatcher matcher = new HashedCredentialsMatcher(AccountService.HASH_ALGORITHM);
-		matcher.setHashIterations(AccountService.HASH_INTERATIONS);
-
-		setCredentialsMatcher(matcher);
-	}
-
 	public void setAccountService(AccountService accountService) {
 		this.accountService = accountService;
 	}
 
-	// /**
-	// * 自定义Authentication对象，使得Subject除了携带用户的登录名外还可以携带更多信息.
-	// */
-	// public static class ShiroUser implements Serializable {
-	// private static final long serialVersionUID = -1373760761780840081L;
-	// public String loginName;
-	// public String name;
-	//
-	// public ShiroUser(String loginName, String name) {
-	// this.loginName = loginName;
-	// this.name = name;
-	// }
-	//
-	// public String getName() {
-	// return name;
-	// }
-	//
-	// /**
-	// * 本函数输出将作为默认的<shiro:principal/>输出.
-	// */
-	// @Override
-	// public String toString() {
-	// return loginName;
-	// }
-	//
-	// /**
-	// * 重载hashCode,只计算loginName;
-	// */
-	// @Override
-	// public int hashCode() {
-	// return Objects.hashCode(loginName);
-	// }
-	//
-	// /**
-	// * 重载equals,只计算loginName;
-	// */
-	// @Override
-	// public boolean equals(Object obj) {
-	// if (this == obj) {
-	// return true;
-	// }
-	// if (obj == null) {
-	// return false;
-	// }
-	// if (getClass() != obj.getClass()) {
-	// return false;
-	// }
-	// ShiroUser other = (ShiroUser) obj;
-	// if (loginName == null) {
-	// if (other.loginName != null) {
-	// return false;
-	// }
-	// } else if (!loginName.equals(other.loginName)) {
-	// return false;
-	// }
-	// return true;
-	// }
-	// }
 }
